c7226cea21c91c59ddc57ec2ed7c3b0b6fb03a53
alaska <rus.rakhmatullin.06@bk.ru>
2025-06-23 20:22:49 +0300

    update 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,30 @@ import { Button } from '@mui/material';
 
 function Header({ isRunning, setIsRunning, setIsModalOpen }) {
   return (
-   <header className="header">
-    <h2>Симуляция Ятсрибы и Голуби</h2>
-      <Button
-        onClick={() => setIsRunning(!isRunning)}
-        className="header-button"
-        variant="contained"
-      >
-       Старт
-      </Button>
-      <Button
-        onClick={() => setIsModalOpen(true)}
-        className="header-button"
-        variant="contained"
-        sx={{
-          backgroundColor: '#cc1414',
-        }}
-      >
-        Настройки
-      </Button>
-    </header>
-  );
+		<header className='header'>
+			<h2>Симуляция Ятсрибы и Голуби</h2>
+			<Button
+				onClick={() => setIsRunning(!isRunning)}
+				className='header-button'
+				variant='contained'
+				sx={{
+					'&:hover': {
+						backgroundColor: '#64a145',
+					},
+				}}>
+				Старт
+			</Button>
+			<Button
+				onClick={() => setIsModalOpen(true)}
+				className='header-button'
+				variant='contained'
+				sx={{
+					backgroundColor: '#cc1414',
+				}}>
+				Настройки
+			</Button>
+		</header>
+	)
 }
 
 export default Header;
